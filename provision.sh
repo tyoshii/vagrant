8720c90d841bdf64d0ec29446046c25cb3806ee8
Takanao Yoshii <tyoshii716@gmail.com>
2016-11-24 19:26:32 +0900

    add yum install file

--- provision.sh
+++ provision.sh
@@ -14,7 +14,7 @@ chkconfig httpd on
 # for php 5.6
 yum -y install epel-release
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-yum -y install --enablerepo=remi,remi-php56 php php-devel php-mbstring php-pdo php-gd
+yum -y install --enablerepo=remi,remi-php56 php php-devel php-mbstring php-pdo php-gd php-xml php-mysql
 
 # php.ini
 cp /etc/php.ini /etc/php.ini.bk
@@ -26,7 +26,8 @@ mv composer.phar /usr/local/bin/composer
 composer global require hirak/prestissimo
 
 
-# mysql
+# mysql 5.6
+yum -y install http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
 yum -y install mysql-server
 service mysqld start
 chkconfig mysqld on
