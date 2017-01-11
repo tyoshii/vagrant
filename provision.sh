c479cfcfb9d065fd304eb8b0f1e142fc2649844d
Takanao Yoshii <t_yoshii@datastadium.co.jp>
2017-01-11 11:17:03 +0900

    add yum common package

--- provision.sh
+++ provision.sh
@@ -1,9 +1,9 @@
 # JST
 cp /usr/share/zoneinfo/Japan /etc/localtime
 
-# yum update
+# yum update and install common package
 yum -y update
-yum -y install vim curl
+yum -y install vim curl bind-utils ansible
 
 
 # httpd
